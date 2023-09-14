11083e1ddde49b8e6d5a9b90880a67477083f44a
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-09-14 21:30:17 +0000

    studied variable in js

---
+++ basics_js/variable_1.js
@@ -0,0 +1,18 @@
+
+const accountId=123344
+let accountEmail="abc@gmail.com"
+var accuontName=("shreejoy")
+accountCity="jaipur"
+let accountState;
+// accountId=2     // not allowed
+
+accountEmail="ac@gamil.com"
+console.log(accountEmail);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountId,accuontName,accountCity,accountState])
+
