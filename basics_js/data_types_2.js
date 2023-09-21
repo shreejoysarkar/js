8eda8c5626fc06a1ee61556c83fe618d6180ed6f
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-09-21 14:32:27 +0000

    learnt about datatypes

---
+++ basics_js/data_types_2.js
@@ -0,0 +1,28 @@
+"use strict"; // -> treat all js code as newer version 
+
+
+// alert(3+3)// we are using node not browser
+
+// code readablility must be high
+console.log (3+2); 
+
+console.log("shreejoy")
+
+let name="shreejoy"
+let age=34
+let isLoggedIn=true
+
+
+// number => 2 to power 53 (range)
+// bigint => if no is too large
+// string => "", ''
+// boolean => true/false
+// null => stand-alone value, representation of empty value.
+// undefined => value not assigned
+// symbol => helps to find the uniqueness of the component
+
+// object 
+
+console.log(typeof "shreejoy"); //helps to know the type of the variable
+console.log(typeof null); // output :- object
+console.log(typeof undefined); // output :- undefined
