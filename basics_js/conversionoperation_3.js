c7dbd92b1feb12154a2f7dc7ff20064f27230641
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-09-22 07:48:24 +0000

    js_comparision

---
+++ basics_js/conversionoperation_3.js
@@ -0,0 +1,65 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+// "33"=>33
+// "33abc" => NaN
+// true=> 1; false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true; 0=>false
+// ""=>false
+// "shr"=>true
+
+let someNumber=23
+let strNo=String(someNumber)
+console.log(strNo)
+console.log(typeof strNo)
+
+// ********************OPERATIONS*************************
+
+let value=3
+let negValue= -value
+console.log(negValue);
+
+console.log(2+1);
+console.log(2-1);
+console.log(2*1);
+console.log(2**1);
+console.log(2/1);
+
+let str1="hello"
+let str2=" shreejoy"
+let str3=str1+str2;
+console.log(str3)
+
+console.log("2"+2)
+console.log(1+"2")
+console.log("1"+2+3);
+console.log(2+1+"2");
+console.log(2+1+"2" + 2+2);
+
+
+
+console.log(+true);
+console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3=1+3
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
