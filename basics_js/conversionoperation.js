6e68cbfd1ad59568d0374a5dc6e017a78bd89b74
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-09-22 07:19:04 +0000

    converison_operation more

--- basics_js/conversionoperation.js
+++ basics_js/conversionoperation.js
@@ -27,3 +27,39 @@ let someNumber=23
 let strNo=String(someNumber)
 console.log(strNo)
 console.log(typeof strNo)
+
+// ********************OPERATIONS*************************
+
+let value=3
+let negValue= -value
+console.log(negValue);
+
+console.log(2+1);
+console.log(2-1);
+console.log(2*1);
+console.log(2**1);
+console.log(2/1);
+
+let str1="hello"
+let str2=" shreejoy"
+let str3=str1+str2;
+console.log(str3)
+
+console.log("2"+2)
+console.log(1+"2")
+console.log("1"+2+3);
+console.log(2+1+"2");
+console.log(2+1+"2" + 2+2);
+
+
+
+console.log(+true);
+console.log(+"")
+
+
+let num1,num2,num3
+num1=num2=num3=1+3
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
