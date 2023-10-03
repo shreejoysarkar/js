d747473f54cfacf8bd4acdf465e0ecef8b386559
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-10-03 09:14:52 +0000

    string datatype

---
+++ basics_js/string_6.js
@@ -0,0 +1,9 @@
+const name='shreejoy'
+const count=40
+
+console.log(name+count+'value');
+//mordern sting input 
+console.log(`hello my name is ${name} and my count is ${count}`);
+
+const gameName=new String("IGI") // another way of defining the string variable
+console.log(gameName[0])
