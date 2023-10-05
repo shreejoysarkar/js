ee3e83c1a5e6a6f4e4fdfd6cf0f6dc870b96d558
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-10-05 05:57:12 +0000

    num

---
+++ basics_js/num_and_maths_7.js
@@ -0,0 +1,13 @@
+const score=400
+console.log(score)
+
+const balance=new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const otherNum= 123.1245
+console.log(otherNum.toPrecision(5))
+
+
