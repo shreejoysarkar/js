47790a13b75a2e96b0f58879daf31164cbfd9f7b
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-10-05 05:44:46 +0000

    string in js

--- basics_js/string_6.js
+++ basics_js/string_6.js
@@ -5,5 +5,30 @@ console.log(name+count+'value');
 //mordern sting input 
 console.log(`hello my name is ${name} and my count is ${count}`);
 
-const gameName=new String("IGI") // another way of defining the string variable
-console.log(gameName[0])
+const gameName=new String("IGi_game") // another way of defining the string variable
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName);
+console.log(gameName.charAt(1))
+
+const newString=gameName.substring(0,5);
+console.log(newString)
+
+const anotherstring=gameName.slice(-3,8)
+console.log(anotherstring)
+
+
+const newStringOne="    shreejoy    "
+console.log(newStringOne);
+console.log(newStringOne.trim())
+
+const url='https://shreejoy.com/shreejoy%20sarkar'
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('shreejoy'))
+console.log(url.includes('rak'))
+
+console.log(gameName.split('-'))
