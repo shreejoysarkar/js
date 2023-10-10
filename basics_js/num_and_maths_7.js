3aabec2bf033a3c1f9b3aa1dd5ee81d1169c6a7d
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-10-10 13:53:02 +0000

    num_maths

--- basics_js/num_and_maths_7.js
+++ basics_js/num_and_maths_7.js
@@ -1,3 +1,5 @@
+// ++++++++++++++++++++++++++Number+++++++++++++++++++++++++++
+
 const score=400
 console.log(score)
 
@@ -10,4 +12,24 @@ console.log(balance.toFixed(2));
 const otherNum= 123.1245
 console.log(otherNum.toPrecision(5))
 
+const hundreds=10000000
+console.log(hundreds.toLocaleString('en-IN'))
 
+
+// +++++++++++++++++++++++MATHS++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.9))
+console.log(Math.min(4,3,2,5,1,10))
+console.log(Math.max(4,3,2,5,1,10))
+
+
+
+console.log(Math.random())  //values bt 0 and 1
+
+const min=10
+const max=20
+
+Math.random()
