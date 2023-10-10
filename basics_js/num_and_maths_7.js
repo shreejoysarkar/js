3c810fc0c44ebbacc3f55979522a9aec77b4781e
shreejoysarkar <134836057+shreejoysarkar@users.noreply.github.com>
2023-10-10 13:58:21 +0000

    num_maths

--- basics_js/num_and_maths_7.js
+++ basics_js/num_and_maths_7.js
@@ -27,9 +27,10 @@ console.log(Math.max(4,3,2,5,1,10))
 
 
 
-console.log(Math.random())  //values bt 0 and 1
+console.log(Math.random());  //values bt 0 and 1
+console.log(Math.floor(Math.random()*10)+1);
 
 const min=10
 const max=20
 
-Math.random()
+console.log(Math.floor(Math.random()*(max-min+1))+min);
